7964cf296160a713a51c2e137598e3c4ace7193b
ToniAnnB <88912180+ToniAnnB@users.noreply.github.com>
2023-12-01 07:59:13 -0500

    Generic uow and consumption comepleted

--- JSandwiches.MVC/IRespository/IConsumUnitOfWork.cs
+++ JSandwiches.MVC/IRespository/IConsumUnitOfWork.cs
@@ -1,12 +1,41 @@
 ﻿using JSandwiches.Models.DTO.FoodDTO;
+using JSandwiches.Models.OrderDTO;
+using JSandwiches.Models.SpecialFeaturesDTO;
 using JSandwiches.Models.UsersDTO;
 
 namespace JSandwiches.MVC.IRespository
 {
     public interface IConsumUnitOfWork
     {
-        IGenConsumRespo<AddressDTO> Address { get; }
-        IGenConsumRespo<AddOnDTO> AddOn { get; }
+        #region Food Related 
+         IGenConsumRespo<MenuItemDTO> MenuItem {get;}
+         IGenConsumRespo<AddOnDTO> AddOn {get;}
+         IGenConsumRespo<MenuItemAddOnDTO> MenuItemAddOn {get;}
+         IGenConsumRespo<ItemCategoryDTO> ItemCategory {get;}
+         IGenConsumRespo<ItemSubCategoryDTO> ItemSubCategory {get;}
+        #endregion
+
+        #region Order related
+         IGenConsumRespo<OrderDTO> Order {get;}
+         IGenConsumRespo<OrderStatusDTO> OrderStatus {get;}
+         IGenConsumRespo<ReceiptDTO> Receipt {get;}
+        #endregion
+
+        #region Special Features related
+         IGenConsumRespo<LoyaltyPointDTO> LoyaltyPoint {get;}
+         IGenConsumRespo<CustomerLoyaltyPointDTO> CustomerLoyaltyPoint {get;}
+         IGenConsumRespo<DealDTO> Deal {get;}
+         IGenConsumRespo<DealSpecificsDTO> DealSpecifics {get;}
+         IGenConsumRespo<RatingDTO> Rating {get;}
+         IGenConsumRespo<MenuItemRatingDTO> MenuItemRating {get;}
+        #endregion
+
+        #region Users related
+         IGenConsumRespo<CustomerDTO> Customer {get;}
+         IGenConsumRespo<AddressDTO> Address {get;}
+         IGenConsumRespo<CustomerAddressDTO> CustomerAddress {get;}
+         IGenConsumRespo<ParishDTO> Parish {get;}
+        #endregion
 
     }
 }
--- JSandwiches.MVC/Respository/ConsumUnitOfWork.cs
+++ JSandwiches.MVC/Respository/ConsumUnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using JSandwiches.Models.DTO.FoodDTO;
+using JSandwiches.Models.Food;
 using JSandwiches.Models.OrderDTO;
 using JSandwiches.Models.SpecialFeaturesDTO;
 using JSandwiches.Models.UsersDTO;
@@ -39,13 +40,40 @@ namespace JSandwiches.MVC.Respository
 
         #region Users related
         private IGenConsumRespo<CustomerDTO> _customer;
-        private IGenConsumRespo<AddressDTO> _addresses;
+        private IGenConsumRespo<AddressDTO> _address;
         private IGenConsumRespo<CustomerAddressDTO> _customerAddress;
         private IGenConsumRespo<ParishDTO> _parish;
         #endregion
 
-        public IGenConsumRespo<AddressDTO> Address => _addresses ??= new GenConsumRespo<AddressDTO>("https://localhost:44381/api/Address", _mapper);
-        public IGenConsumRespo<AddOnDTO> AddOn => _addOn ??= new GenConsumRespo<AddOnDTO>("https://localhost:44381/api/AddOn", _mapper);
+        #region Food Related 
+        public IGenConsumRespo<MenuItemDTO> MenuItem => _menuItem??= new GenConsumRespo<MenuItemDTO>("https://localhost:44381/api/MenuItem", _mapper);
+        public IGenConsumRespo<AddOnDTO> AddOn => _addOn??= new GenConsumRespo<AddOnDTO>("https://localhost:44381/api/AddOn", _mapper);
+        public IGenConsumRespo<MenuItemAddOnDTO> MenuItemAddOn => _menuItemAddOn??= new GenConsumRespo<MenuItemAddOnDTO>("https://localhost:44381/api/MenuItemAddOn", _mapper);
+        public IGenConsumRespo<ItemCategoryDTO> ItemCategory => _itemCategory??= new GenConsumRespo<ItemCategoryDTO>("https://localhost:44381/api/ItemCategory", _mapper);
+        public IGenConsumRespo<ItemSubCategoryDTO> ItemSubCategory => _itemSubCategory??= new GenConsumRespo<ItemSubCategoryDTO>("https://localhost:44381/api/ItemSubCategory", _mapper);
+        #endregion
+
+        #region Order related
+        public IGenConsumRespo<OrderDTO> Order => _order??= new GenConsumRespo<OrderDTO>("https://localhost:44381/api/Order", _mapper);
+        public IGenConsumRespo<OrderStatusDTO> OrderStatus => _orderStatus??= new GenConsumRespo<OrderStatusDTO>("https://localhost:44381/api/OrderStatus", _mapper);
+        public IGenConsumRespo<ReceiptDTO> Receipt=> _receipt??= new GenConsumRespo<ReceiptDTO>("https://localhost:44381/api/Receipt", _mapper);
+        #endregion
+
+        #region Special Features related
+        public IGenConsumRespo<LoyaltyPointDTO> LoyaltyPoint => _loyaltyPoint??= new GenConsumRespo<LoyaltyPointDTO>("https://localhost:44381/api/LoyaltyPoint", _mapper);
+        public IGenConsumRespo<CustomerLoyaltyPointDTO> CustomerLoyaltyPoint =>  _customerLoyaltyPoint??= new GenConsumRespo<CustomerLoyaltyPointDTO>("https://localhost:44381/api/CustomerLoyaltyPoint", _mapper);
+        public IGenConsumRespo<DealDTO> Deal => _deal??= new GenConsumRespo<DealDTO>("https://localhost:44381/api/Deal", _mapper);
+        public IGenConsumRespo<DealSpecificsDTO> DealSpecifics => _dealSpecifics??= new GenConsumRespo<DealSpecificsDTO>("https://localhost:44381/api/DealSpecifics", _mapper);
+        public IGenConsumRespo<RatingDTO> Rating => _rating??= new GenConsumRespo<RatingDTO>("https://localhost:44381/api/Rating", _mapper);
+        public IGenConsumRespo<MenuItemRatingDTO> MenuItemRating => _menuItemRating??= new GenConsumRespo<MenuItemRatingDTO>("https://localhost:44381/api/MenuItemRating", _mapper);
+        #endregion
+
+        #region Users related
+        public IGenConsumRespo<CustomerDTO> Customer => _customer??= new GenConsumRespo<CustomerDTO>("https://localhost:44381/api/Customer", _mapper);
+        public IGenConsumRespo<AddressDTO> Address => _address??= new GenConsumRespo<AddressDTO>("https://localhost:44381/api/Address", _mapper);
+        public IGenConsumRespo<CustomerAddressDTO> CustomerAddress => _customerAddress??= new GenConsumRespo<CustomerAddressDTO>("https://localhost:44381/api/CustomerAddress", _mapper);
+        public IGenConsumRespo<ParishDTO> Parish => _parish??= new GenConsumRespo<ParishDTO>("https://localhost:44381/api/Parish", _mapper);
+        #endregion
 
     }
 }
