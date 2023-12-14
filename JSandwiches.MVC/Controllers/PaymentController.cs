555d30cd14114f77bf9c067e30c297b0d664d145
ToniAnnB <88912180+ToniAnnB@users.noreply.github.com>
2023-12-14 18:40:26 -0500

    minor changes

---
+++ JSandwiches.MVC/Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JSandwiches.MVC.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
